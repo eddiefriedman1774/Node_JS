a973d4d4332355d8039b9dc96c380edd28a6406b
eddiefriedman1774 <eddiefriedman1@gmail.com>
2023-06-02 19:55:10 -0400

    added example to index.js

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+      type: 'confirm',
+      name: 'bacon',
+      message: 'Do you like bacon?',
+    },
+    {
+      type: 'input',
+      name: 'favorite',
+      message: 'Bacon lover, what is your favorite type of bacon?',
+      when(answers) {
+        return answers.bacon;
+      },
+    },
+    
+  ];
+  
+//this is an object
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+       
+    inquirer.prompt(questions).then(/* ... */);  
 
-// Function call to initialize app
+    
+    // inquirer.prompt(questions).then((answers) => {
+    //     console.log(JSON.stringify(answers, null, '  '));
+    //   });
+
+}
+
 init();
