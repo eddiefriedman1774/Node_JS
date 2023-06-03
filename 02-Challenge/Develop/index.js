1a87d592ba5a95dc6466c6061cd27492e3dd5e4e
eddiefriedman1774 <eddiefriedman1@gmail.com>
2023-06-02 20:54:34 -0400

    added backup to see if it would work when I put the questions in

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,42 +1,50 @@
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
-
+var fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
 
 const questions = [
-    {
-      type: 'confirm',
-      name: 'bacon',
-      message: 'Do you like bacon?',
-    },
-    {
-      type: 'input',
-      name: 'favorite',
-      message: 'Bacon lover, what is your favorite type of bacon?',
-      when(answers) {
-        return answers.bacon;
-      },
-    },
-    
-  ];
+
+  {
+    type: 'input',
+    name: 'Title',
+    message: 'What is the project title?'
+  },
+  {
+    type: 'input',
+    name: 'Description',
+    message: 'What is the project description?'
+  },
+  {
+    type: 'input',
+    name: 'Installation_instructions',
+    message: 'How do you install this?'
+  }
+];
   
 //this is an object
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
+
+
+
 
 // TODO: Create a function to initialize app
 function init() { 
        
-    inquirer.prompt(questions).then(/* ... */);  
+   
+    inquirer.prompt(questions).then((answers) => {
+      var text = []
+      text.push("# "+ answers.Title)  
+      writeToFile("backup.md",text.join("\n"))
 
-    
-    // inquirer.prompt(questions).then((answers) => {
-    //     console.log(JSON.stringify(answers, null, '  '));
-    //   });
+    });
 
 }
-
 init();
+// writeToFile("Test","This is a test");
